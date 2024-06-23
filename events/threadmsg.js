23ec5c8edc5f86c3f66ef7d0ebd81eb16b7cb248
spectralo <arthur.hardy@mageekbox.net>
2024-06-23 16:29:25 +0200

    Fixed memory

--- events/threadmsg.js
+++ events/threadmsg.js
@@ -1,8 +1,7 @@
 const { Events } = require('discord.js');
 require('dotenv').config();
 require('node-fetch');
-const openai_token = "Bearer " + process.env.OPENAI
-const huggingface_token = "Bearer " + process.env.HUGGINGFACE
+const openai_token = "Bearer " + process.env.OPENAI;
 
 module.exports = {
   name: Events.MessageCreate,
@@ -11,20 +10,22 @@ module.exports = {
     if (message.channel.isThread() && message.channel.name.includes("Conversation with")) {
       if (message.author.bot) return;
       // get old messages there
-      history = "Voici la question de l'utilisateur, répondez-y : " + message.content + ", Ces messages sont votre mémoire des derniers messages. Ne répondez jamais aux questions ici !. Si un utilisateur vous pose une question, comme quel est son nom verifiez ici qu'il n'y soit pas présent  :"
+      let history = [
+      ]
 
-      message.channel.messages.fetch({ limit: 10 }).then(messages => {
+      await message.channel.messages.fetch({ limit: 10 }).then(messages => {
         messages.forEach(element => {
           if (element.author.bot) {
-            history = history.concat("Bot:"+element.content+", ")
-          }
-          else {
-            history = history.concat("User:"+element.author.username + " : " + element.content+", ")
+            history.push({"role": "assistant", "content": element.content});
+          } else {
+            history.push({"role": "user", "content": element.content});
           }
         });
 
       })
       .catch(console.error);
+      history.push({"role": "user", "content": message.content});
+      
 
       fetch('https://jamsapi.hackclub.dev/openai/chat/completions', {
         method: 'POST', //GET, POST, PUT, DELETE
@@ -36,19 +37,14 @@ module.exports = {
         body: JSON.stringify(
           {
             'model': 'gpt-3.5-turbo',
-            'messages': [
-              {
-                'role': 'user',
-                'content': history,
-              }
-            ],
+            'messages': history
           }
         )
       })
         .then(result => result.json())
         .then(response => {
           message.reply(response.choices[0].message.content)
-        })
+        });
     }
   },
 };
